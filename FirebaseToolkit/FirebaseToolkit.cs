86eb221ed2dc841623a19dc8413c60691ff9239b
5argon <pyasry@gmail.com>
2017-08-09 23:48:53 +0700

    Added preprocessor to be able to test in real device. Removed sudo system.

--- FirebaseToolkit/FirebaseToolkit.cs
+++ FirebaseToolkit/FirebaseToolkit.cs
@@ -4,10 +4,11 @@ using Firebase.Database;
 using Firebase.Auth;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.IO;
 using UnityEngine;
-#if UNITY_EDITOR
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
 using Firebase.Unity.Editor;
 #endif
 
@@ -18,15 +19,20 @@ using Firebase.Unity.Editor;
 /// <returns></returns>
 public abstract class FirebaseToolkit<ITSELF> where ITSELF : FirebaseToolkit<ITSELF>, new()
 {
-    private static readonly string defaultInstanceName = "mydefault";
-    private static readonly string sudoInstanceName = "sudo";
+    private static readonly string defaultInstanceName = "default";
+
     /// <summary>
     /// Format : "gs://my-custom-bucket"
+    /// For storage you can't set a new default from Firebase. I think setting here is easier than changing the config file manually.
     /// </summary>
     protected abstract string BucketName { get; }
 
+
+//I will really make sure all of these does not leak out of your computer..!
+#if UNITY_EDITOR
     /// <summary>
     /// Format : "https://yourgame.firebaseio.com/"
+    /// DatabaseUrl can't be read off the config file in editor. You must provide the string for editor use.
     /// </summary>
     protected abstract string DatabaseUrl { get; }
 
@@ -40,8 +46,14 @@ public abstract class FirebaseToolkit<ITSELF> where ITSELF : FirebaseToolkit<ITS
     /// </summary>
     protected abstract string ServiceAccountEmail { get; }
 
+    /// <summary>
+    /// In editor you can't use Auth, therefore you can't get UID from Firebase starting from e-mail and password.
+    /// This dict will transform e-mail to UID. It indicates all the possible login you can do from the editor.
+    /// </summary>
+    public abstract Dictionary<string, string> EditorEmailToUID { get; }
+#endif
+
     //All the C# abstract cannot be static so those are useless to the abstract methods.. because of this we have to make a singleton of FirebaseToolkit just for accessing those abstracts. lol
-    //But this is internal use singleton, so it is private.
     protected static ITSELF instance;
     protected static ITSELF Instance
     {
@@ -55,25 +67,62 @@ public abstract class FirebaseToolkit<ITSELF> where ITSELF : FirebaseToolkit<ITS
         }
     }
 
-    /// <summary>
-    /// The reason why I don't use FirebaseApp.DefaultInstance is that I want to be able to re-instantiate my "default" in the test.
-    /// </summary>
-    private static FirebaseApp defaultFirebaseApp;
-    protected static FirebaseApp DefaultFirebaseApp
+#if UNITY_EDITOR
+    private static string currentInstanceName = defaultInstanceName;
+    private static List<string> createdInstanceList = new List<string>() { defaultInstanceName };
+    private static void SwitchInstance(string toName)
+    {
+        if (createdInstanceList.Contains(toName) == false)
+        {
+            Debug.Log("Created new FirebaseApp instance named " + toName);
+            FirebaseApp.Create(DefaultFirebaseOption, toName);
+            createdInstanceList.Add(toName);
+        }
+
+        if (currentInstanceName == toName)
+        {
+            throw new Exception("We should not have switch to the same instance! Stupid! (" + currentInstanceName + "->" + toName + ")");
+        }
+
+        currentInstanceName = toName;
+
+        //Reset cache so all of them gets a new one from the ground up
+        currentFirebaseApp = null;
+        database = null;
+        storage = null;
+// #if !UNITY_EDITOR
+//         auth = null;
+// #endif
+    }
+#endif
+
+    private static FirebaseApp currentFirebaseApp;
+    public static FirebaseApp CurrentFirebaseApp
     {
         get
         {
-            if (defaultFirebaseApp == null)
+            if (currentFirebaseApp == null)
             {
-                FirebaseApp.Create(FirebaseApp.DefaultInstance.Options, defaultInstanceName);
-                defaultFirebaseApp = FirebaseApp.GetInstance(defaultInstanceName);
+#if UNITY_EDITOR
+                currentFirebaseApp = FirebaseApp.GetInstance(currentInstanceName);
+#else
+                currentFirebaseApp = FirebaseApp.DefaultInstance;
+#endif
             }
-            return defaultFirebaseApp;
+            return currentFirebaseApp;
         }
     }
 
+    private static AppOptions DefaultFirebaseOption
+    {
+        get { return FirebaseApp.DefaultInstance.Options; }
+    }
+
+    //I prevent the use of Auth in the editor since the state after using it is very unpredictable
+    //See my research : https://gametorrahod.com/unity-firebase-realtime-database-gotchas-4359ff576026
+#if !UNITY_EDITOR
     private static FirebaseAuth auth;
-    protected static FirebaseAuth Auth 
+    public static FirebaseAuth Auth 
     { 
         get 
         {
@@ -84,71 +133,24 @@ public abstract class FirebaseToolkit<ITSELF> where ITSELF : FirebaseToolkit<ITS
             return auth;
         }
     }
-
-    public static bool IsLoggedIn
-    {
-        get{
-#if UNITY_EDITOR
-            if(
-                DefaultFirebaseApp.GetEditorServiceAccountEmail() != "" &&
-                DefaultFirebaseApp.GetEditorAuthUserId() != "" &&
-                DefaultFirebaseApp.GetEditorAuthUserId() != null)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-#else
-            return (Auth.CurrentUser != null && Auth.CurrentUser.UserId != "");
 #endif
-        }
-    }
-
-    protected static string UserEmail
-    {
-        get{
-            if(IsLoggedIn== false)
-            {
-                throw new Exception("Cannot get E-mail while not logged in");
-            }
-#if UNITY_EDITOR
-            return DefaultFirebaseApp.GetEditorServiceAccountEmail();
-#else
-            return Auth.CurrentUser.Email;
-#endif
-        }
-    }
-
-	protected static string UserID 
-	{
-		get
-		{
-            if(IsLoggedIn == false)
-            {
-                throw new Exception("Cannot get ID while not logged in");
-            }
-#if UNITY_EDITOR
-            return DefaultFirebaseApp.GetEditorAuthUserId();
-#else
-            return Auth.CurrentUser.UserId;
-#endif
-		}
-	}
 
     private static FirebaseDatabase database;
-    public static FirebaseDatabase Database 
-    { 
-        get 
+    public static FirebaseDatabase Database
+    {
+        get
         {
-            if(database == null)
+            if (database == null)
             {
 #if UNITY_EDITOR
-                DefaultFirebaseApp.SetEditorDatabaseUrl(Instance.DatabaseUrl);
-                return FirebaseDatabase.GetInstance(DefaultFirebaseApp);
+                if (IsLoggedIn == false)
+                {
+                    throw new System.Exception("No! You must login before using the database in editor!");
+                }
+                CurrentFirebaseApp.SetEditorDatabaseUrl(Instance.DatabaseUrl);
+                database = FirebaseDatabase.GetInstance(CurrentFirebaseApp);
 #else
-                database = FirebaseDatabase.GetInstance(Instance.DatabaseUrl);
+                database = FirebaseDatabase.GetInstance(CurrentFirebaseApp);
 #endif
             }
             return database;
@@ -162,65 +164,117 @@ public abstract class FirebaseToolkit<ITSELF> where ITSELF : FirebaseToolkit<ITS
         {
             if (storage == null)
             {
-                storage = FirebaseStorage.GetInstance(Instance.BucketName);
+                storage = FirebaseStorage.GetInstance(CurrentFirebaseApp);
             }
             return storage;
         }
     }
 
 #if UNITY_EDITOR
-    private static FirebaseApp sudoFirebaseApp;
-    private static FirebaseApp SudoFirebaseApp
+    /// <summary>
+    /// Call this from your Login in Editor instead of Auth. Maybe use #if UNITY_EDITOR to help.
+    /// </summary>
+    protected static void EditorLogin(string username, string password, string uid)
     {
-        get
-        {
-            if (sudoFirebaseApp == null)
-            {
-                FirebaseApp.Create(FirebaseApp.DefaultInstance.Options, sudoInstanceName);
-                sudoFirebaseApp = FirebaseApp.GetInstance(sudoInstanceName);
-                sudoFirebaseApp.SetEditorP12FileName(Instance.P12FileName);
-                sudoFirebaseApp.SetEditorServiceAccountEmail(Instance.ServiceAccountEmail);
-                sudoFirebaseApp.SetEditorP12Password("notasecret");
-            }
-            return sudoFirebaseApp;
-        }
-    }
+        //In editor... to support freely switching user, each username would need a separate Firebase instance
+        //since you cannot take it out once you made the first DB call.
 
-    private static FirebaseDatabase sudoDatabase;
-    public static FirebaseDatabase SudoDatabase 
-    { 
-        get 
-        {
-            if(sudoDatabase == null)
-            {
-                SudoFirebaseApp.SetEditorDatabaseUrl(Instance.DatabaseUrl);
-                sudoDatabase = FirebaseDatabase.GetInstance(SudoFirebaseApp);
-            }
-            return sudoDatabase;
-        }
-    }
+        //In the real game we would need only one instance.
+        SwitchInstance(username); //Every CurrentFirebaseApp will change!
 
-    protected static void EditorLogin(string username, string password,string uid)
-    {
+        CurrentFirebaseApp.SetEditorP12FileName(Instance.P12FileName);
+        CurrentFirebaseApp.SetEditorServiceAccountEmail(Instance.ServiceAccountEmail);
+        CurrentFirebaseApp.SetEditorP12Password("notasecret");
+        CurrentFirebaseApp.SetEditorAuthUserId(uid);
 
-        DefaultFirebaseApp.SetEditorP12FileName(Instance.P12FileName);
-        DefaultFirebaseApp.SetEditorServiceAccountEmail(Instance.ServiceAccountEmail);
-        DefaultFirebaseApp.SetEditorP12Password("notasecret");
-        DefaultFirebaseApp.SetEditorAuthUserId(uid);
-
-        Debug.LogFormat("EDITOR login {0}", uid);
+        Debug.LogFormat("EDITOR login as {0}", uid);
     }
 #endif
 
-    public static void LogCurrentUser()
+    public static void Logout()
     {
-        if (IsLoggedIn)
-        {
-            Debug.Log(string.Format("UID : {0} Email : {1}", UserID, UserEmail));
+#if UNITY_EDITOR
+        SwitchInstance(defaultInstanceName);
+#else
+        Auth.SignOut();
+#endif
+    }
+
+    public static string LoginInformation
+    {
+        get{
+            string info = 
+            @"UserId : {0}
+            E-mail : {1}
+            PlayerId : {2}
+            FormattedPlayerId : {3}";
+
+            #if UNITY_EDITOR
+            return string.Format(info,
+            CurrentFirebaseApp.GetEditorAuthUserId(),
+            currentInstanceName,
+            PlayerData.Local.PlayerId,
+            PlayerData.Local.FormattedShortPlayerId);
+            #else
+            return string.Format(info,
+            Auth.CurrentUser.Email,
+            Auth.CurrentUser.UserId,
+            PlayerData.Local.PlayerId,
+            PlayerData.Local.FormattedShortPlayerId);
+            #endif
         }
-        else
+    }
+
+
+    public static bool IsLoggedIn
+    {
+        get
         {
-            Debug.Log("Currently not logged in..");
+#if UNITY_EDITOR
+            if (currentInstanceName != defaultInstanceName)
+            {
+                return true;
+            }
+            else
+            {
+                //We have made sure the default ones can't have login information even in editor.
+                return false;
+            }
+#else
+            return (Auth.CurrentUser != null && Auth.CurrentUser.UserId != "");
+#endif
+        }
+    }
+
+    protected static string UserEmail
+    {
+        get
+        {
+            if (IsLoggedIn == false)
+            {
+                throw new Exception("Cannot get E-mail while not logged in");
+            }
+#if UNITY_EDITOR
+            return CurrentFirebaseApp.Name; //The instance name is an e-mail
+#else
+            return Auth.CurrentUser.Email;
+#endif
+        }
+    }
+
+    protected static string UserID
+    {
+        get
+        {
+            if (IsLoggedIn == false)
+            {
+                throw new Exception("Cannot get ID while not logged in");
+            }
+#if UNITY_EDITOR
+            return CurrentFirebaseApp.GetEditorAuthUserId();
+#else
+            return Auth.CurrentUser.UserId;
+#endif
         }
     }
 
@@ -257,7 +311,7 @@ public abstract class FirebaseToolkit<ITSELF> where ITSELF : FirebaseToolkit<ITS
                 }
             }
             //Cannot use "using" since it is based on callback. The stream will already be disposed by then.
-            stream.Dispose(); 
+            stream.Dispose();
         });
 
     }
--- IntegrationTest/InteBase.cs
+++ IntegrationTest/InteBase.cs
@@ -163,3 +163,25 @@ public abstract class InteBase {
 
 
 }
+
+/// <summary>
+/// Test with this attribute runs only in Unity editor
+/// </summary>
+public class UnityEditorPlatformAttribute : UnityPlatformAttribute
+{
+    public UnityEditorPlatformAttribute()
+    {
+        this.include = new RuntimePlatform[]{RuntimePlatform.WindowsEditor, RuntimePlatform.OSXEditor, RuntimePlatform.LinuxEditor};
+    }
+}
+
+/// <summary>
+/// Test with this attribute runs only on the real mobile device
+/// </summary>
+public class UnityMobilePlatformAttribute : UnityPlatformAttribute
+{
+    public UnityMobilePlatformAttribute()
+    {
+        this.include = new RuntimePlatform[]{RuntimePlatform.Android, RuntimePlatform.IPhonePlayer};
+    }
+}
