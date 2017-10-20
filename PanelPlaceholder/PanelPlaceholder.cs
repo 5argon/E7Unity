9866b23c001066f8966b746a5bcc571f1dd26313
5argon <pyasry@gmail.com>
2017-10-20 12:50:40 +0700

    Added PanelPlaceholder.

---
+++ PanelPlaceholder/PanelPlaceholder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class PanelPlaceholder : MonoBehaviour {
+
+	private RectTransform rectTransform;
+	public bool overrideScale;
+
+	public void ReplaceWith(RectTransform panel)
+	{
+		rectTransform = GetComponent<RectTransform>();
+
+		panel.transform.SetParent(gameObject.transform.parent,true);
+        panel.transform.SetSiblingIndex(gameObject.transform.GetSiblingIndex());
+		panel.anchoredPosition = rectTransform.anchoredPosition;
+		panel.localPosition = rectTransform.localPosition;
+		if(overrideScale)
+		{
+			panel.localScale = rectTransform.localScale;
+		}
+		Destroy(gameObject);
+	}
+
+}
---
+++ PanelPlaceholder/README.md
@@ -0,0 +1,5 @@
+# PanelPlaceholder
+
+Sometimes you have to assemble UI parts later and you could not use a prefab. (It might already contains some prefab you want to preserve the link)
+
+By creating an empty GameObject with this script, you can replace it later with a panel. It will automatically expand the size to fit parent and stay at the exact index that the placeholder GameObject was.
