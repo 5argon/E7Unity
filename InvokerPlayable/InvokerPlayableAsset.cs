08167251a8ea54526206cf7685c5fd28a14072e5
5argon <pyasry@gmail.com>
2018-02-23 17:36:46 +0700

    Fix bug of Invoker where it invokes nothing at scene start.

--- InvokerPlayable/InvokerPlayableAsset.cs
+++ InvokerPlayable/InvokerPlayableAsset.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.Timeline;
 using UnityEngine.UI;
 using System;
 
@@ -11,7 +12,7 @@ public interface INamedInvoker
 }
 
 [System.Serializable]
-public abstract class InvokerPlayableAsset<T> : PlayableAsset, INamedInvoker where T : UnityEngine.Object
+public abstract class InvokerPlayableAsset<T> : PlayableAsset, ITimelineClipAsset, INamedInvoker where T : UnityEngine.Object
 {
     [SerializeField] private ExposedReference<T> invokeTarget;
 
@@ -19,6 +20,11 @@ public abstract class InvokerPlayableAsset<T> : PlayableAsset, INamedInvoker whe
 
     protected abstract Enum selectedMethod { get; }
     protected abstract Dictionary<Enum, Action> MethodPairings(T t);
+
+    public ClipCaps clipCaps
+    {
+        get { return ClipCaps.None; }
+    }
 
     public override Playable CreatePlayable(PlayableGraph graph, GameObject go)
     {
@@ -30,6 +36,8 @@ public abstract class InvokerPlayableAsset<T> : PlayableAsset, INamedInvoker whe
             if (MethodPairings(resolvedInvokeTarget).TryGetValue(selectedMethod, out action))
             {
                 invoker.InvokeOnPlay = action;
+                invoker.EnumName = selectedMethod.ToString();
+                //Debug.Log("Creating Playable " + selectedMethod.ToString());
                 return ScriptPlayable<InvokerPlayableBehaviour>.Create(graph, invoker);
             }
             else
@@ -37,6 +45,7 @@ public abstract class InvokerPlayableAsset<T> : PlayableAsset, INamedInvoker whe
                 Debug.LogError($"The enum {selectedMethod} is not paired with any delegate.");
             }
         }
+        //Debug.LogWarning("Cannot resolve the exposed reference.");
         return Playable.Create(graph);
     }
 }
--- InvokerPlayable/InvokerPlayableBehaviour.cs
+++ InvokerPlayable/InvokerPlayableBehaviour.cs
@@ -7,12 +7,13 @@ using System;
 public class InvokerPlayableBehaviour : PlayableBehaviour
 {
     public Action InvokeOnPlay { set; private get; }
+    public string EnumName { set; private get; }
 
 	public override void OnBehaviourPlay(Playable playable, FrameData info) {
-		//Debug.Log("Invoking...");
+		//Debug.Log("Invoking " + EnumName);
 		if(Application.isPlaying)
 		{
-			InvokeOnPlay.Invoke();
+			InvokeOnPlay?.Invoke();
 		}
 	}
 }
