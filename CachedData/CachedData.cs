10514d0eb46a861cc59625af9ec8881247bf34e7
5argon <pyasry@gmail.com>
2018-03-03 16:19:16 +0700

    Add an interface to CachedData

--- CachedData/CachedData.cs
+++ CachedData/CachedData.cs
@@ -3,16 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class CachedData<T> {
+/// <summary>
+/// Common point of all cacheables, so you can program something without knowing its T generic class type.
+/// </summary>
+public interface ICacheableData
+{
+    bool IsDirty { get; }
+    void SetDirty();
+}
+
+public class CachedData<T> : ICacheableData {
 
     private T data;
+
     private bool dirty;
+    public bool IsDirty => dirty;
+    public void SetDirty() => dirty = true;
 
     public CachedData()
     {
         SetDirty();
     }
 
+    /// <summary>
+    /// Data is allowed to get only if it is not dirty, which is achieved by setting something to it.
+    /// Data can be set anytime, doing so will remove the dirty status.
+    /// </summary>
     public T Data
     {
         get
@@ -28,16 +44,6 @@ public class CachedData<T> {
             data = value;
             dirty = false; //clean! not dirty!
         }
-    }
-
-    public bool IsDirty
-    {
-        get{ return dirty; }
-    }
-
-    public void SetDirty()
-    {
-        dirty = true;
     }
 
 }
