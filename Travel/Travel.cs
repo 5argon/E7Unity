175bfe4d637d114582a07a93b83c880efd5a1257
5argon <pyasry@gmail.com>
2018-05-13 13:55:19 +0700

    ECS-struct based travel!

--- Travel/Travel.cs
+++ Travel/Travel.cs
@@ -2,30 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Transforms2D;
+using Unity.Jobs;
+using Unity.Collections;
+
+public struct TravelData<T> : IComponentData
+{
+    public NativeArray<TravelEvent<T>> EventList;
+}
+
+public struct TravelRememberIndex : IComponentData
+{
+    public int rememberIndex;
+}
+
 public class Travel<T>
 {
-    private List<TravelEvent<T>> EventList { get; }
+    private Entity entityToEventList;
+
+    /// <summary>
+    /// Would be most people's EM that would like the data to be in.
+    /// </summary>
+    private static EntityManager EntityManager => World.Active.GetExistingManager<EntityManager>();
+
+    private static EntityArchetype archetype;
+    private static void InitializeManager()
+    {
+        archetype = EntityManager.CreateArchetype(typeof(TravelData<T>), typeof(TravelRememberIndex));
+    }
 
     public Travel()
     {
-        EventList = new List<TravelEvent<T>>();
+        if(!archetype.Valid)
+        {
+            InitializeManager();
+        }
+
+        //When creating a "Travel" we could allocate things in the manager instead.
+        //This Travel then could be an interface to the entity.
+        entityToEventList = EntityManager.CreateEntity(archetype);
     }
 
-    public TravelEvent<T> FirstEvent => EventList.Count == 0 ? null : EventList[0];
-    public TravelEvent<T> LastEvent => EventList.Count == 0 ? null : EventList[EventList.Count - 1];
+    private TravelData<T> GetTravelData => EntityManager.GetComponentData<TravelData<T>>(entityToEventList);
+    private TravelRememberIndex GetRememberIndex => EntityManager.GetComponentData<TravelRememberIndex>(entityToEventList);
+    private NativeArray<TravelEvent<T>> GetEventList => GetTravelData.EventList;
 
-    private int rememberIndex = 0;
+    public TravelEvent<T> FirstEvent => GetEventList[0];
+    public TravelEvent<T> LastEvent => GetEventList[GetEventList.Length - 1];
+    public bool NoEvent => GetEventList.Length == 0;
 
     /// <summary>
     /// Get the most recent event before the position on the timeline.
     /// </summary>
     public TravelEvent<T> EventOfPosition(float position)
     {
+        NativeArray<TravelEvent<T>> EventList = GetTravelData.EventList;
+        TravelRememberIndex tri = GetRememberIndex;
+        int rememberIndex = tri.rememberIndex;
+
         //return Find((te) => te.IsPositionInRange(position));
         int plusIndex = -1;
         int minusIndex = 0;
         int useIndex;
-        for (int i = 0; i < EventList.Count; i++)
+        for (int i = 0; i < EventList.Length; i++)
         {
             if ((i % 2 != 0))
             {
@@ -42,7 +84,7 @@ public class Travel<T>
             }
             else
             {
-                if(rememberIndex + (plusIndex + 1) < EventList.Count)
+                if(rememberIndex + (plusIndex + 1) < EventList.Length)
                 {
                     plusIndex++;
                     useIndex = rememberIndex + plusIndex;
@@ -59,10 +101,15 @@ public class Travel<T>
             {
                 rememberIndex = useIndex;
                 //Debug.Log($"Ok we got {EventList[useIndex]}");
+
+                //Save rememberIndex back to entity..
+                tri.rememberIndex = rememberIndex;
+                EntityManager.SetComponentData<TravelRememberIndex>(entityToEventList, tri);
+
                 return EventList[useIndex];
             }
         }
-        return null;
+        return TravelEvent<T>.INVALID;
     }
 
     /// <summary>
@@ -70,16 +117,19 @@ public class Travel<T>
     /// </summary>
     public TravelEvent<T> EventOfTime(float time)
     {
-        for(int i = 0; i < EventList.Count ; i++)
+        for(int i = 0; i < GetEventList.Length; i++)
         {
-            if(EventList[i].IsTimeInRange(time))
+            if(GetEventList[i].IsTimeInRange(time))
             {
-                return EventList[i];
+                return GetEventList[i];
             }
         }
-        return null;
+        return TravelEvent<T>.INVALID;
     }
 
+    /// <summary>
+    /// Left this as non-ECS?
+    /// </summary>
     private bool HasEventAtZero { get; set; }
 
     /// <summary>
@@ -95,20 +145,36 @@ public class Travel<T>
         }
     }
 
-
     /// <summary>
     /// TIME IS TIME ELAPSED NOT ANY TIME YOU WANT!!
     /// </summary>
     public void Add(float position, float timeElapsed, T data)
     {
         //Debug.Log($"Adding {position} {timeElapsed} {data.ToString()}");
-        if (EventList.Count != 0 && timeElapsed <= 0)
+        if (GetEventList.Length != 0 && timeElapsed <= 0)
         {
             throw new System.Exception($"Time elapsed except the first one must be positive. position : {position} timeElapsed : {timeElapsed}");
         }
-        TravelEvent<T> travelEvent = new TravelEvent<T>(position, (LastEvent?.Time ?? 0) + timeElapsed, data);
-        LastEvent?.LinkToNext(travelEvent);
-        EventList.Add(travelEvent);
+        TravelEvent<T> travelEvent = new TravelEvent<T>(position, (NoEvent ? 0 : LastEvent.Time) + timeElapsed, data);
+        if(!NoEvent)
+        {
+            LastEvent.LinkToNext(travelEvent);
+        }
+
+        //Super costly Add haha
+        TravelData<T> td = GetTravelData;
+        NativeArray<TravelEvent<T>> eventList = td.EventList;
+        NativeArray<TravelEvent<T>> biggerEventList = new NativeArray<TravelEvent<T>>(eventList.Length + 1, Allocator.Persistent);
+        for(int i = 0; i < eventList.Length; i++)
+        {
+            biggerEventList[i] = eventList[i];
+        }
+        //Finally add the new one...
+        biggerEventList[biggerEventList.Length-1] = travelEvent;
+        eventList.Dispose();
+        td.EventList = biggerEventList;
+        EntityManager.SetComponentData<TravelData<T>>(entityToEventList, td);
+
         if(position == 0)
         {
             HasEventAtZero = true;
@@ -119,74 +185,74 @@ public class Travel<T>
 /// <summary>
 /// In addition to position and time, call .Data to get any kind of data you want.
 /// </summary>
-public class TravelEvent<T>
+public struct TravelEvent<T> 
 {
-        public float Position { get; }
-        /// <summary>
-        /// When this is the last event, this is Mathf.Infinity
-        /// </summary>
-        public float PositionNext { get; private set; }
+    private static readonly TravelEvent<T> constINVALID = new TravelEvent<T>(Mathf.NegativeInfinity, float.NegativeInfinity, default(T));
+    public static ref readonly TravelEvent<T> INVALID => ref constINVALID;
 
-        public float Time { get; }
-        /// <summary>
-        /// When this is the last event, this is Mathf.Infinity
-        /// </summary>
-        public float TimeNext { get; private set;}
+    public bool Invalid => Position == constINVALID.Position && Time == constINVALID.Time;
+    public bool Valid => !Invalid;
 
-        public T Data {get;}
-        /// <summary>
-        /// When this is the last event, this is null.
-        /// </summary>
-        public TravelEvent<T> Next {get; private set;}
-        public TravelEvent<T> Previous {get; private set;}
-
-        public TravelEvent(float absolutePosition, float time, T data)
-        {
-            this.Position = absolutePosition;
-            this.PositionNext = Mathf.Infinity;
-            this.Time = time;
-            this.TimeNext = Mathf.Infinity;
-            this.Data = data;
-        }
-
-        public void LinkToNext(TravelEvent<T> te)
-        {
-            this.TimeNext = te.Time;
-            this.PositionNext = te.Position;
-            this.Next = te;
-            te.Previous = this;
-            //Debug.Log("Link result : " + this.ToString());
-        }
-
-        public bool IsPositionInRange(float position)
-        {
-            //Debug.Log($"Is in range? {Position} - {position} - {PositionNext}");
-            return (position >= Position) && (position < PositionNext);
-        }
-
-        public bool IsTimeInRange(float time)
-        {
-            //Debug.LogFormat("Time Range is {0} - {1}",Time,TimeNext);
-            return (time >= Time) && (time < TimeNext);
-        }
-
-        public override string ToString()
-        {
-            return string.Format($"P {Position}-{PositionNext} T {Time}-{TimeNext} {Data.ToString()}");
-        }
-}
-
-public static class ListExtension
-{
+    public float Position { get; }
     /// <summary>
-    /// WIP
+    /// When this is the last event, this is Mathf.Infinity
     /// </summary>
-    public static T ParallelFind<T>(this List<T> list, int startIndex, System.Predicate<T> criteria)
+    public float PositionNext { get; private set; }
+
+    public float Time { get; }
+    /// <summary>
+    /// When this is the last event, this is Mathf.Infinity
+    /// </summary>
+    public float TimeNext { get; private set; }
+
+    public T Data { get; }
+    /// <summary>
+    /// When this is the last event, this is null.
+    /// </summary>
+
+    public TravelEvent<T> Next
     {
-        int listCount = list.Count;
-        for (int i = 0; i < listCount; i++)
-        {
-        }
-        return list[0];
+        get => throw new System.NotImplementedException();
+        set => throw new System.NotImplementedException();
+    }
+    public TravelEvent<T> Previous
+    {
+        get => throw new System.NotImplementedException();
+        set => throw new System.NotImplementedException();
+    }
+
+    public TravelEvent(float absolutePosition, float time, T data)
+    {
+        this.Position = absolutePosition;
+        this.PositionNext = Mathf.Infinity;
+        this.Time = time;
+        this.TimeNext = Mathf.Infinity;
+        this.Data = data;
+    }
+
+    public void LinkToNext(TravelEvent<T> te)
+    {
+        this.TimeNext = te.Time;
+        this.PositionNext = te.Position;
+        this.Next = te;
+        te.Previous = this;
+        //Debug.Log("Link result : " + this.ToString());
+    }
+
+    public bool IsPositionInRange(float position)
+    {
+        //Debug.Log($"Is in range? {Position} - {position} - {PositionNext}");
+        return (position >= Position) && (position < PositionNext);
+    }
+
+    public bool IsTimeInRange(float time)
+    {
+        //Debug.LogFormat("Time Range is {0} - {1}",Time,TimeNext);
+        return (time >= Time) && (time < TimeNext);
+    }
+
+    public override string ToString()
+    {
+        return string.Format($"P {Position}-{PositionNext} T {Time}-{TimeNext} {Data.ToString()}");
     }
 }
