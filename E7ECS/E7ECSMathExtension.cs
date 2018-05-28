3def291261a6ebe9bceaf3614f29180dcf551984
5argon <pyasry@gmail.com>
2018-05-28 18:54:26 +0700

    Added RectTransform to screen rect.

--- E7ECS/E7ECSMathExtension.cs
+++ E7ECS/E7ECSMathExtension.cs
@@ -1,4 +1,5 @@
 using Unity.Mathematics;
+using UnityEngine;
 
 namespace E7.ECS
 {
@@ -10,6 +11,21 @@ namespace E7.ECS
                 return math.clamp((value - a) / (b - a), 0, 1);
             else
                 return 0.0f;
+        }
+
+        /// <summary>
+        /// Cannot be used in a Job!
+        /// RectTransform has to be aligned completely flat to the screen. Discards all Z information. The purpose is to use `Rect` in a job because it is a struct.
+        /// </summary>
+        public static Rect ScreenRectOfRectTransform(RectTransform rt, Camera c)
+        {
+            Vector3[] fc = new Vector3[4];
+            rt.GetWorldCorners(fc);
+            for (int a = 0; a < 3; a++)
+            {
+                fc[a] = Camera.main.WorldToScreenPoint(fc[a]);
+            }
+            return new Rect(fc[0].x, fc[0].y, fc[2].x - fc[1].x, fc[1].y - fc[0].y);
         }
     }
 }
