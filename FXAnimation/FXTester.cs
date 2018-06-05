fbab8d304d0d556fa4b1fd4b2291cfbbe9deda6b
5argon <pyasry@gmail.com>
2018-06-06 00:57:30 +0700

    Added FXAnimation.

---
+++ FXAnimation/FXTester.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class FXTester : MonoBehaviour {
+
+	public FXAnimation effect;
+	private Vector3 remember;
+
+	void Start()
+	{
+		remember = effect.transform.position;
+	}
+
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.Z))
+		{
+			effect.Play(remember,false,false,false);
+		}
+		if(Input.GetKeyDown(KeyCode.X))
+		{
+			effect.Stop();
+		}
+	
+	}
+}
---
+++ FXAnimation/README.md
@@ -0,0 +1,5 @@
+# FXAnimation
+
+The unsophisicated frame-by-frame animator. Works by directly on-offing the game object.
+
+The very core of flavors and style of Duel Otters, it is 90% more efficient than Animator and about 50% more efficient than legacy Animation.
